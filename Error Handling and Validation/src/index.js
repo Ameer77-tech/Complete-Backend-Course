2d1717382f0ce1ef19c124ece9af40eecc4bcdf3
Ameer77-tech <ameershaik.cs@gmail.com>
2026-06-30 13:26:00 +0530

    feat: implement custom error handling middleware and error classes for improved error management

--- Error Handling and Validation/src/index.js
+++ Error Handling and Validation/src/index.js
@@ -1,7 +1,32 @@
 import server from "./server.js";
 import env from "./config/env.js";
+import prisma from "./config/prisma.js";
 
 const PORT = env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Running on PORT ${PORT}`);
+
+async function start() {
+  try {
+    await prisma.$connect();
+
+    console.log("✅ Database connected");
+
+    server.listen(PORT, () => {
+      console.log(`🚀 Server started on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Failed to connect to database:", err);
+    process.exit(1);
+  }
+}
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+start();
--- Error Handling and Validation/src/server.js
+++ Error Handling and Validation/src/server.js
@@ -1,4 +1,6 @@
 import express, { urlencoded } from "express";
+import NotFoundError from "./errors/NotFoundError.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 const server = express();
 
 server.use(express.json());
@@ -7,5 +9,11 @@ server.use(urlencoded({ extended: true }));
 server.get("/api/v1/health", (req, res) => {
   res.send("<b>Running</b>");
 });
+
+server.use((req, res, next) => {
+  next(new NotFoundError("Route Not Found"));
+});
+
+server.use(errorMiddleware);
 
 export default server;
